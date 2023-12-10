5808b1a76a5362a5ff487859c59854cc5d31b4dc
Chumak Nikita <117525795+Nikita-devel@users.noreply.github.com>
2023-12-10 18:16:38 +0100

    Update pictures.py
    API-маршрут для додавання тегів до світлин

--- killer_instagram/src/repository/pictures.py
+++ killer_instagram/src/repository/pictures.py
@@ -1,10 +1,28 @@
 from typing import List
 
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 
 from src.schemas.pictures import PictureModel
 from src.database.models import Picture, User, Tag, Comment
+from src.services.auth import service_auth
+from src.database.db import get_db
+from src.repository import pictures as repository_pictures
+from src.schemas import pictures as schema_picture
 
+router = APIRouter(prefix='/pictures', tags=['pictures'])
+
+@router.post('/add_tags/{picture_id}', response_model=schema_picture.PictureResponce,
+             status_code=status.HTTP_200_OK)
+async def add_tags_to_picture(
+    picture_id: int,
+    tags: List[str] = Query(..., title="Tags to add"),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(service_auth.get_current_user)
+):
+    picture = await repository_pictures.add_tags_to_picture(picture_id, tags, db)
+    return picture
+    
 
 async def create_picture(body: PictureModel, url: str, user: User, db: Session) -> Picture:
     picture = Picture(description=body.description, url=url, user_id=user.id)
