328205f4b2074934859e2e7566fd612654295e72
Chumak Nikita <117525795+Nikita-devel@users.noreply.github.com>
2023-12-10 18:23:06 +0100

    Update pictures.py
    Добавлення генерації qr-code

--- killer_instagram/src/routes/pictures.py
+++ killer_instagram/src/routes/pictures.py
@@ -15,10 +15,12 @@ from fastapi import (
 from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.orm import Session
 
+from fastapi import APIRouter, HTTPException
+from starlette.responses import StreamingResponse
 from src.services.auth import service_auth
 from src.services.cloud_photos import CloudImage
 from src.database.db import get_db
-from src.database.models import User
+from src.database.models import User, Picture
 from src.repository import pictures as repository_pictures
 from src.schemas import pictures as schema_picture
 
@@ -46,3 +48,24 @@ async def read_photos(skip: int = 0, limit: int = 10, db: Session = Depends(get_
                       current_user: User = Depends(service_auth.get_current_user)):
     pictures = await repository_pictures.read_pictures(skip=skip, limit=limit, user=current_user, db=db)
     return pictures
+
+@router.get('/transformed/{picture_id}', response_class=StreamingResponse)
+async def get_transformed_picture(picture_id: int):
+    picture = await repository_pictures.get_picture_by_id(picture_id, db)
+    if not picture:
+        raise HTTPException(status_code=404, detail="Picture not found")
+    
+    transformed_url = CloudImage.get_transformed_url(picture.url)
+    image_content = CloudImage.download(transformed_url)
+    
+    return StreamingResponse(content=image_content, media_type="image/jpeg")
+
+@router.get('/qr-code/{picture_id}', response_class=StreamingResponse)
+async def get_qr_code(picture_id: int):
+    picture = await repository_pictures.get_picture_by_id(picture_id, db)
+    if not picture:
+        raise HTTPException(status_code=404, detail="Picture not found")
+
+    qr_code_content = generate_qr_code(picture.url)  #  функція для генерації QR-коду
+
+    return StreamingResponse(content=qr_code_content, media_type="image/png")
