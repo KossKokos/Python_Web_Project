e7443cf1c33e491a8eedaf05d85c5e668f8f9527
MikeIV2007 <130536460+MikeIV2007@users.noreply.github.com>
2023-12-14 23:22:19 +0200

    after review_2

--- killer_instagram/src/services/logout.py
+++ killer_instagram/src/services/logout.py
@@ -15,14 +15,12 @@ class LogoutDependency:
                        current_user: User = Depends(service_auth.get_current_user),
                        credentials: HTTPAuthorizationCredentials = Security(security),
                        db: Session = Depends(get_db)):
-        print(18,request.method, request.url)
-        print(19,f"user_id: {current_user.id}")
+
         user_id = current_user.id
         token = db.query(BlacklistedToken).filter(BlacklistedToken.user_id == user_id).first()
         access_token = credentials.credentials
-        print (24, access_token)
+        
         if token and token.blacklisted_token == access_token:
-            print( 26, token.blacklisted_token)
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail=f"Operation forbidden for {current_user.email}. Please login again!"
